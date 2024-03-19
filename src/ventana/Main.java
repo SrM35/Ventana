f4275695fa58ca039fe0ae74fb5fd43506056d1b
SrM35 <jrojo_22@alu.uabcs.mx>
2024-03-18 17:55:38 -0700

    Unidad 3 Actividad 6

--- src/ventana/Main.java
+++ src/ventana/Main.java
@@ -3,7 +3,8 @@ package ventana;
 public class Main {
 
 	public static void main(String[] args) {
-		ventana ventan = new ventana();
+		//ventana ventan = new ventana();
+		Ventana2 ventan = new Ventana2();
 	}
 
 }
---
+++ src/ventana/Ventana2.java
@@ -0,0 +1,73 @@
+package ventana;
+
+import java.awt.*;
+
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import com.toedter.calendar.JDateChooser;
+
+public class Ventana2 extends JFrame{
+	public Ventana2() {
+		this.setLayout(null);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(700, 700);
+
+		this.setTitle("Boton");
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setMinimumSize(new Dimension(300, 300));
+		this.loadComponents();
+		repaint();
+
+	}
+	
+	public void loadComponents() {
+		boton();
+	}
+	
+	public void boton() {
+		Random rand = new Random();
+		
+		JButton boton = new JButton("Puchame!");
+		boton.setBounds(225, 500, 200, 90);
+		boton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int valorX = rand.nextInt(650);
+				int valorY = rand.nextInt(650);
+				int valorW = rand.nextInt(200);
+				int valorH = rand.nextInt(200);
+				
+				int valorR = rand.nextInt(255);
+				int valorG = rand.nextInt(255);
+				int valorB = rand.nextInt(255);
+				
+				JButton btn = new JButton("Hola Mundo");
+				btn.setBounds(valorX, valorY, valorW, valorH);
+				btn.setBackground(new Color(valorR, valorG, valorB));
+				add(btn);
+				btn.updateUI();
+				
+			}
+			
+		});
+		getContentPane().add(boton);
+		
+		JPanel fondo = new JPanel();
+		fondo.setBounds(0, 0, 700, 700);
+		fondo.setBackground(Color.GREEN);
+		add(fondo);
+	}
+	
+}
